40a3bce03192980363103c8b047ccd5f768168d2
张森 <864486270@qq.com>
2018-06-24 10:51:47 +0800

    test1

---
+++ testpod/testpod/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testpod
+//
+//  Created by 张森 on 2018/6/24.
+//  Copyright © 2018年 ZhangSen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testpod/testpod/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  testpod
+//
+//  Created by 张森 on 2018/6/24.
+//  Copyright © 2018年 ZhangSen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
